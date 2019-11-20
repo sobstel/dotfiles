a6b0c4c135b56238d4ebc683e36eefa1cf62233a
Przemek Sobstel <przemek@sobstel.org>
2019-11-20 13:04:44 +0100

    update

--- LaunchAgents/startup.sh
+++ LaunchAgents/startup.sh
@@ -3,3 +3,7 @@
 # remove old downloads
 find /Users/sobstel/Downloads -maxdepth 1 -mtime +5 -exec rm -rf {} \;
 
+# remove redundant xcode data
+find /Users/sobstel/Library/Developer/Xcode/DerivedData -maxdepth 1 -mtime +14 -exec rm -rf {} \;
+find /Users/sobstel/Library/Developer/CoreSimulator/Devices -maxdepth 1 -type d -mtime +30 -exec rm -rf {} \;
+
